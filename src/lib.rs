eb2c560b03408f8bd227bdd8f87b969698c85749
Orhan "aib" Kavrakoglu <aibok42@gmail.com>
2022-12-18 17:12:34 +0300

    Make u8_to_display_char private and remove its unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,7 @@ pub fn get_hexdump_row(bytes: &[u8], bytes_per_row: usize) -> String {
 	row
 }
 
-pub fn u8_to_display_char(u: u8) -> char {
+fn u8_to_display_char(u: u8) -> char {
 	if u.is_ascii_graphic() {
 		char::from(u)
 	} else if u == 32 {
@@ -232,31 +232,6 @@ mod tests {
 			get_hexdump_row(b" .\x00\x01\x02\x03", 9),
 			"20 2e 00 01 02 03            ....."
 		);
-	}
-
-	#[test]
-	fn test_u8_to_display_char() {
-		assert_eq!(u8_to_display_char(0x00), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x01), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x09), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x0a), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x1f), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x20), ' ');
-		assert_eq!(u8_to_display_char(0x21), '!');
-		assert_eq!(u8_to_display_char(0x22), '"');
-		assert_eq!(u8_to_display_char(0x27), '\'');
-		assert_eq!(u8_to_display_char(0x3f), '?');
-		assert_eq!(u8_to_display_char(0x40), '@');
-		assert_eq!(u8_to_display_char(0x41), 'A');
-		assert_eq!(u8_to_display_char(0x5a), 'Z');
-		assert_eq!(u8_to_display_char(0x5c), '\\');
-		assert_eq!(u8_to_display_char(0x61), 'a');
-		assert_eq!(u8_to_display_char(0x7a), 'z');
-		assert_eq!(u8_to_display_char(0x7e), '~');
-		assert_eq!(u8_to_display_char(0x7f), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x80), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0x81), SUBSTITUTE_CHAR);
-		assert_eq!(u8_to_display_char(0xff), SUBSTITUTE_CHAR);
 	}
 
 	#[test]
