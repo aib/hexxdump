fd8e1d74e72bd3e3248c0d7679bacc077f92fc28
Orhan "aib" Kavrakoglu <aibok42@gmail.com>
2022-12-18 17:27:07 +0300

    Add hexdump_with_width, a custom bytes-per-row version of hexdump

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ pub fn hexdump(bytes: &[u8]) {
 	hexdump_to(std::io::stdout(), bytes, 16).ok();
 }
 
+pub fn hexdump_with_width(bytes: &[u8], bytes_per_row: usize) {
+	hexdump_to(std::io::stdout(), bytes, bytes_per_row).ok();
+}
+
 pub fn hexdump_to<W: std::io::Write>(mut output: W, bytes: &[u8], bytes_per_row: usize) -> std::io::Result<usize> {
 	let mut written = 0;
 	for (address, row) in get_rows(bytes, bytes_per_row) {
